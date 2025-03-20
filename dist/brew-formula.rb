88cb15a3e352969e5ccfdc5d88a5165eca32cbcf
Antti Harju <antti@harju.io>
2025-03-20 20:23:20 +0200

    Import brew formula from actions monorepo
    File extension .rb because that gives the best syntax
    highlighting.

---
+++ dist/brew-formula.rb
@@ -0,0 +1,25 @@
+class $CLASS_NAME < Formula
+    desc "$DESCRIPTION"
+    homepage "$HOMEPAGE"
+
+    url "$URL"
+    sha256 "$SHA256"
+    head "https://$REPOSITORY"
+
+    depends_on "go@$GO_VERSION" => :build
+
+    def install
+        ENV["GOPATH"] = buildpath
+
+        bin_path = buildpath/"src/$REPOSITORY"
+        bin_path.install Dir["*"]
+        cd bin_path do
+          system "go", "build", "-ldflags", "-s -w -buildid=brew-$VERSION"
+          bin.install "$APP_NAME"
+        end
+    end
+
+    test do
+        system "true"
+    end
+end
