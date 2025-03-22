53889767c39f0a6d74a9582bc67bd80cf740d947
Antti Harju <git@anttiharju.dev>
2025-03-22 23:36:54 +0200

    Alternate ref

--- .github/actions/get-version/action.sh
+++ .github/actions/get-version/action.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Normalise working directory
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
 source ../../../dist/brew/values.sh
 
 echo "version=$version" >> "$GITHUB_OUTPUT"
