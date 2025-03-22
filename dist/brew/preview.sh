56e85c2f3553815271ea7d6a8aebbcbd07bc872a
Antti Harju <git@anttiharju.dev>
2025-03-22 20:26:26 +0200

    Cleanup

--- dist/brew/preview.sh
+++ dist/brew/preview.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
+# Normalise working directory
+cd "$(dirname "${BASH_SOURCE[0]}")"
 
 export GITHUB_REPOSITORY=anttiharju/vmatch
 
--- dist/brew/values.sh
+++ dist/brew/values.sh
@@ -1,25 +1,18 @@
 #!/usr/bin/env bash
 
-app_name=$(basename "$GITHUB_REPOSITORY")
-class_name=$(awk 'BEGIN{print toupper(substr("'"$app_name"'",1,1)) substr("'"$app_name"'",2)}')
-description=$(gh repo view  --json description --jq .description)
-homepage=$(gh api "repos/$GITHUB_REPOSITORY" --jq .homepage)
+capture() {
+    eval "$1=\"$2\""
+    echo "$1 = $2"
+}
 
-url=$(gh api "repos/$GITHUB_REPOSITORY/releases/latest" --jq .tarball_url)
-sha256=$(curl -sL "$url" | shasum -a 256 | cut -d ' ' -f1)
-repository="github.com/$GITHUB_REPOSITORY"
-
-go_version=$(go list -m -f '{{.GoVersion}}' | awk -F. '{print $1"."$2}')
-version=$(basename "$url")
-
-echo "$app_name"
-echo "$class_name"
-echo "$description"
-echo "$homepage"
-
-echo "$url"
-echo "$sha256"
-echo "$repository"
-
-echo "$go_version"
-echo "$version"
+repo_name="$(basename "$GITHUB_REPOSITORY")"
+capture app_name "$repo_name"
+capture class_name "$(awk 'BEGIN{print toupper(substr("'"$repo_name"'",1,1)) substr("'"$repo_name"'",2)}')"
+capture description "$(gh repo view --json description --jq .description)"
+capture homepage "$(gh api "repos/$GITHUB_REPOSITORY" --jq .homepage)"
+url="$(gh api "repos/$GITHUB_REPOSITORY/releases/latest" --jq .tarball_url)"
+capture url "$url"
+capture sha256 "$(curl -sL "$url" | shasum -a 256 | cut -d ' ' -f1)"
+capture repository "github.com/$GITHUB_REPOSITORY"
+capture go_version "$(go list -m -f '{{.GoVersion}}' | awk -F. '{print $1"."$2}')"
+capture version "$(basename "$url")"
