7df35c1237967b37c0f972f6b85b8b900b8ea9dd
Antti Harju <antti@harju.io>
2025-03-22 20:56:48 +0200

    Fix Ruby indentation, following Rubocop instructions

--- dist/brew/formula.tmpl.rb
+++ dist/brew/formula.tmpl.rb
@@ -1,25 +1,25 @@
 class ${class_name} < Formula
-    desc "${description}"
-    homepage "${homepage}"
+  desc "${description}"
+  homepage "${homepage}"
 
-    url "${url}"
-    sha256 "${sha256}"
-    head "https://${repository}"
+  url "${url}"
+  sha256 "${sha256}"
+  head "https://${repository}"
 
-    depends_on "go@${go_version}" => :build
+  depends_on "go@${go_version}" => :build
 
-    def install
-        ENV["GOPATH"] = buildpath
+  def install
+    ENV["GOPATH"] = buildpath
 
-        bin_path = buildpath/"src/${repository}"
-        bin_path.install Dir["*"]
-        cd bin_path do
-          system "go", "build", "-ldflags", "-s -w -buildid=brew-${version}"
-          bin.install "${app_name}"
-        end
+    bin_path = buildpath/"src/${repository}"
+    bin_path.install Dir["*"]
+    cd bin_path do
+      system "go", "build", "-ldflags", "-s -w -buildid=brew-${version}"
+      bin.install "${app_name}"
     end
+  end
 
-    test do
-        system "true"
-    end
+  test do
+    system "true"
+  end
 end
