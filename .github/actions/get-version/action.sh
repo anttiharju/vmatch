2e823e7aadf854daa1ca08d0a8009569434fc844
Antti Harju <git@anttiharju.dev>
2025-03-22 23:14:14 +0200

    Remove shellcheck disable

--- .github/actions/get-version/action.sh
+++ .github/actions/get-version/action.sh
@@ -3,9 +3,4 @@ set -euo pipefail
 
 source ../../../dist/brew/values.sh
 
-if [[ -z "${version-}" ]]; then
-    echo "Error: version variable not set by values.sh" >&2
-    exit 1
-fi
-
 echo "version=$version" >> "$GITHUB_OUTPUT"
