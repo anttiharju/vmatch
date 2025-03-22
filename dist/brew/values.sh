acdc8f3ad979baea058a7188f10e017c86c8d27f
Antti Harju <antti@harju.io>
2025-03-22 20:12:46 +0200

    Improve cuddling

--- dist/brew/values.sh
+++ dist/brew/values.sh
@@ -10,15 +10,16 @@ sha256=$(curl -sL "$url" | shasum -a 256 | cut -d ' ' -f1)
 repository="github.com/$GITHUB_REPOSITORY"
 
 go_version=$(go list -m -f '{{.GoVersion}}' | awk -F. '{print $1"."$2}')
-
 version=$(basename "$url")
 
 echo "$app_name"
 echo "$class_name"
 echo "$description"
 echo "$homepage"
+
 echo "$url"
 echo "$sha256"
 echo "$repository"
+
 echo "$go_version"
 echo "$version"
