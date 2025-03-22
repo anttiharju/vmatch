84a660ef21064cbd7f8cd949de609df84a231c01
Antti Harju <git@anttiharju.dev>
2025-03-22 22:25:06 +0200

    Add missing EditorConfig hook

--- dist/brew/preview.sh
+++ dist/brew/preview.sh
@@ -6,8 +6,8 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 formula_file="formula.tpl"
 if [ ! -f "$formula_file" ]; then
-  echo "Template file is missing: $formula_file"
-  exit 1
+    echo "Template file is missing: $formula_file"
+    exit 1
 fi
 
 # Mock relevant parts of GitHub Actions env
@@ -20,11 +20,11 @@ quick_mode=false
 
 set -a
 if [[ "$quick_mode" == true ]] && [[ -f "$cache_file" ]]; then
-  echo "Using cached values from $cache_file"
-  cat "$cache_file"
+    echo "Using cached values from $cache_file"
+    cat "$cache_file"
 else
-  echo "Generating fresh values"
-  source values.sh | tee "$cache_file"
+    echo "Generating fresh values"
+    source values.sh | tee "$cache_file"
 fi
 # Cache file only exists if this script has been ran
 # shellcheck disable=SC1090
