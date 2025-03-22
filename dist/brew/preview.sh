27650689a9e882f768b577ee892e76bb00de066d
Antti Harju <git@anttiharju.dev>
2025-03-22 20:38:43 +0200

    More comments

--- dist/brew/preview.sh
+++ dist/brew/preview.sh
@@ -7,6 +7,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 # Match what is available in GitHub Actions
 export GITHUB_REPOSITORY="anttiharju/vmatch"
 
+# Mimic anttiharju/actions/render-template
 set -a
 source values.sh
 set +a
