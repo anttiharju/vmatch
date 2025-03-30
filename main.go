c9f7757df2cf1494b6b33944b241fc5d560dcd91
Antti Harju <antti@harju.io>
2025-03-30 14:17:56 +0300

    Rename picker to choose

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"os"
 
+	"github.com/anttiharju/vmatch/pkg/choose"
 	"github.com/anttiharju/vmatch/pkg/interrupt"
-	"github.com/anttiharju/vmatch/pkg/picker"
 )
 
 func main() {
 	go interrupt.Listen(os.Interrupt)
 
-	exitCode := picker.SelectWrapper(os.Args[1:])
+	exitCode := choose.Wrapper(os.Args[1:])
 	os.Exit(exitCode)
 }
