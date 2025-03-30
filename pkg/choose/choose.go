a8982c0845bb6b7ca8df7fadba2609bf71c87aea
Antti Harju <antti@harju.io>
2025-03-30 19:04:07 +0300

    Prepare by adding ctx drilling

--- pkg/choose/choose.go
+++ pkg/choose/choose.go
@@ -1,6 +1,8 @@
 package choose
 
 import (
+	"context"
+
 	"github.com/anttiharju/vmatch/pkg/language"
 	"github.com/anttiharju/vmatch/pkg/linter"
 )
@@ -9,17 +11,17 @@ func firstArgIs(arg string, args []string) bool {
 	return len(args) > 0 && args[0] == arg
 }
 
-func Wrapper(args []string) int {
+func Wrapper(ctx context.Context, args []string) int {
 	if firstArgIs("go", args) {
 		wrappedLanguage := language.Wrap("go")
-		exitCode := wrappedLanguage.Run(args[1:])
+		exitCode := wrappedLanguage.Run(ctx, args[1:])
 
 		return exitCode
 	}
 
 	if firstArgIs("golangci-lint", args) {
 		wrappedLinter := linter.Wrap("golangci-lint")
-		exitCode := wrappedLinter.Run(args[1:])
+		exitCode := wrappedLinter.Run(ctx, args[1:])
 
 		return exitCode
 	}
