436b637c592ecab5c6b3a0e0b5cd2013f45e157f
Antti Harju <antti@harju.io>
2025-05-25 17:36:58 +0300

    Fix platform_arch extraction

--- dist/brew/bottle.bash
+++ dist/brew/bottle.bash
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 file="$1"
-platform_arch=$(basename "$file" | grep -o '[^.]*\.[^.]*\.bottle' | sed 's/\.bottle$//' | sed 's/^vmatch-[0-9]*\.//')
+platform_arch=$(basename "$file" | sed -E 's/^vmatch-[0-9]+\.[0-9]+\.[0-9]+\.//' | sed -E 's/\.bottle.*$//')
 hash=$(shasum -a 256 "$file" | cut -d ' ' -f1)
 
 echo "    sha256 cellar: :any, $platform_arch: '$hash'"
