12fbd1e5939e6fa35e02ec4671c32b5fb80c8a95
Antti Harju <antti@harju.io>
2025-05-25 10:21:00 +0300

    Scaffolding

--- internal/choose/choose.go
+++ internal/choose/choose.go
@@ -3,6 +3,7 @@ package choose
 import (
 	"context"
 
+	"github.com/anttiharju/vmatch/internal/inject"
 	"github.com/anttiharju/vmatch/internal/language"
 	"github.com/anttiharju/vmatch/internal/linter"
 )
@@ -12,6 +13,8 @@ func firstArgIs(arg string, args []string) bool {
 }
 
 func Wrapper(ctx context.Context, args []string) int {
+	inject.Scripts()
+
 	if firstArgIs("go", args) {
 		wrappedLanguage := language.Wrap("go")
 		exitCode := wrappedLanguage.Run(ctx, args[1:])
