5d8c6d0395ace1ee925652a40ef714752b161b10
anttiharju <antti@harju.io>
2025-10-26 21:24:42 +0100

    Fix comment

--- pkgs/render.sh
+++ pkgs/render.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")" # normalize working directory so caller wd does not matter
 
-# Validate PKG as enum
+# Validate pkg as enum
 pkg="${1:-}"
 case "$pkg" in
   brew)
