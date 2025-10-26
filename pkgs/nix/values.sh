b9a5ef04ec8e61daab6575001cf1aae1be758059
anttiharju <antti@harju.io>
2025-10-26 12:24:04 +0100

    Improve nix/value.sh

--- pkgs/nix/values.sh
+++ pkgs/nix/values.sh
@@ -11,17 +11,9 @@ capture() {
   echo "$1=\"$2\""
 }
 
-REPO_ROOT="$(git rev-parse --show-toplevel)"
-
 repo_name="$(basename "$GITHUB_REPOSITORY")"
 echo "repo_name=\"$repo_name\""
 
-capture class_name "$(awk 'BEGIN{print toupper(substr("'"$repo_name"'",1,1)) substr("'"$repo_name"'",2)}')"
-capture description "$(gh repo view --json description --jq .description)"
-capture homepage "$(gh api "repos/$GITHUB_REPOSITORY" --jq .homepage)"
 capture version "${TAG#v}"
 capture repo_owner "${GITHUB_REPOSITORY%%/*}"
-capture darwin_amd64_sha256 "$(shasum -a 256 "$REPO_ROOT/$repo_name-darwin-amd64.tar.gz" | cut -d ' ' -f1)"
-capture darwin_arm64_sha256 "$(shasum -a 256 "$REPO_ROOT/$repo_name-darwin-arm64.tar.gz" | cut -d ' ' -f1)"
-capture linux_amd64_sha256 "$(shasum -a 256 "$REPO_ROOT/$repo_name-linux-amd64.tar.gz" | cut -d ' ' -f1)"
-capture linux_arm64_sha256 "$(shasum -a 256 "$REPO_ROOT/$repo_name-linux-arm64.tar.gz" | cut -d ' ' -f1)"
+# TODO: nix_sha256
