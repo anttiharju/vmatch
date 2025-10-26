55485f5c61b59d0b2e6fbe4a0266ba2fc4e51b76
anttiharju <antti@harju.io>
2025-10-26 12:54:29 +0100

    Remove extra cd

--- pkgs/github/release.sh
+++ pkgs/github/release.sh
@@ -13,5 +13,4 @@ rm -rf "tmp/$os-$arch"
 repo_name="$(basename "$(pwd)")"
 CGO_ENABLED=0 go build -ldflags "-s -w -buildid=github-$version" -trimpath -o "tmp/$os-$arch/$repo_name"
 
-cd "tmp/$os-$arch"
-tar -czf "$repo_root/$repo_name-$os-$arch.tar.gz" "$repo_name"
+tar -czf "$repo_name-$os-$arch.tar.gz" "tmp/$os-$arch/$repo_name"
