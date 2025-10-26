2d488a6c165ce21d1a6591ff319985e1798a7ba4
anttiharju <antti@harju.io>
2025-10-26 12:56:14 +0100

    Change repo_name parsing to be more robust

--- pkgs/github/release.sh
+++ pkgs/github/release.sh
@@ -8,7 +8,7 @@ arch="$3"
 echo "./pkgs/github/release.sh $version $os $arch"
 
 rm -rf "tmp/$os-$arch"
-repo_name="$(basename "$(pwd)")"
+repo_name="$(basename -s .git "$(git remote get-url origin)")"
 CGO_ENABLED=0 go build -ldflags "-s -w -buildid=github-$version" -trimpath -o "tmp/$os-$arch/$repo_name"
 
 tar -czf "$repo_name-$os-$arch.tar.gz" "tmp/$os-$arch/$repo_name"
