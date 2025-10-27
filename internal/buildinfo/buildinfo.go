8e2002166944e1c4763846f76310b8fec7785980
anttiharju <antti@harju.io>
2025-10-27 19:04:52 +0100

    Create a type

--- internal/buildinfo/buildinfo.go
+++ internal/buildinfo/buildinfo.go
@@ -10,6 +10,24 @@ import (
 	"github.com/anttiharju/vmatch/internal/exitcode"
 )
 
+type BuildInfo struct {
+	version string
+	time    string
+	rev     string
+}
+
+func (b BuildInfo) Version() string {
+	return b.version
+}
+
+func (b BuildInfo) Time() string {
+	return b.time
+}
+
+func (b BuildInfo) Rev() string {
+	return b.rev
+}
+
 func Print() exitcode.Exitcode {
 	if buildInfo, ok := debug.ReadBuildInfo(); ok {
 		version := strings.TrimPrefix(buildInfo.Main.Version, "v")
