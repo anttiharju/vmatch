284dd4617ccf3bbcd21f7d46ccb306205a443401
anttiharju <antti@harju.io>
2025-10-27 19:24:24 +0100

    Turn ok into a guard clause

--- internal/buildinfo/print.go
+++ internal/buildinfo/print.go
@@ -11,46 +11,47 @@ import (
 )
 
 func Print(info BuildInfo) exitcode.Exitcode {
-	if buildInfo, ok := debug.ReadBuildInfo(); ok {
-		version := strings.TrimPrefix(buildInfo.Main.Version, "v")
-		goVersion := buildInfo.GoVersion
-
-		var revision, buildTime string
-
-		for _, setting := range buildInfo.Settings {
-			switch setting.Key {
-			case "vcs.revision":
-				revision = setting.Value
-			case "vcs.time":
-				buildTime = setting.Value
-			}
-		}
-
-		infoRev := info.Revision()
-		if infoRev != "" {
-			revision = infoRev
-		}
-
-		infoVersion := info.Version()
-		if infoVersion != "" {
-			version = infoVersion
-		}
-
-		infoTime := info.Time()
-		if infoTime != "" {
-			buildTime = infoTime
-		}
-
-		const maxLength = 8
-		if len(revision) > maxLength {
-			revision = revision[:maxLength]
-		}
-
-		programName := filepath.Base(os.Args[0])
-		fmt.Printf("%s has version %s built with %s from %s on %s\n", programName, version, goVersion, revision, buildTime)
-	} else {
+	buildInfo, ok := debug.ReadBuildInfo()
+	if !ok {
 		return exitcode.VersionError
 	}
+
+	version := strings.TrimPrefix(buildInfo.Main.Version, "v")
+	goVersion := buildInfo.GoVersion
+
+	var revision, buildTime string
+
+	for _, setting := range buildInfo.Settings {
+		switch setting.Key {
+		case "vcs.revision":
+			revision = setting.Value
+		case "vcs.time":
+			buildTime = setting.Value
+		}
+	}
+
+	infoRev := info.Revision()
+	if infoRev != "" {
+		revision = infoRev
+	}
+
+	infoVersion := info.Version()
+	if infoVersion != "" {
+		version = infoVersion
+	}
+
+	infoTime := info.Time()
+	if infoTime != "" {
+		buildTime = infoTime
+	}
+
+	const maxLength = 8
+	if len(revision) > maxLength {
+		revision = revision[:maxLength]
+	}
+
+	programName := filepath.Base(os.Args[0])
+	fmt.Printf("%s has version %s built with %s from %s on %s\n", programName, version, goVersion, revision, buildTime)
 
 	return exitcode.Success
 }
