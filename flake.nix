2196c19d86e7dbd86d1356e1e7f49a018b24211b
anttiharju <antti@harju.io>
2025-10-28 21:45:00 +0100

    Use Go 1.24.6

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Go development environment";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
   };
 
   outputs =
