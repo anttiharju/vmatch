e2d311a3c39020d06f7b00346b19a6db2e4cd5c3
Antti Harju <antti@harju.io>
2025-11-09 14:46:41 +0200

    Update nix packaging

--- .release/nix/template.nix
+++ .release/nix/template.nix
@@ -1,23 +1,37 @@
-{ buildGoModule, fetchFromGitHub }:
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
 
-buildGoModule {
+buildGoModule rec {
   pname = "${PKG_REPO}";
   version = "${PKG_VERSION}";
+  revision = "${PKG_REV}";
 
   src = fetchFromGitHub {
     owner = "${PKG_OWNER}";
     repo = "${PKG_REPO}";
-    rev = "${PKG_REV}";
+    rev = revision;
     hash = "${PKG_HASH}";
   };
+
+  vendorHash = null;
 
   ldflags = [
     "-s"
     "-w"
-    "-X main.revision=${PKG_REV}"
-    "-X main.version=${PKG_VERSION}"
+    "-X main.revision=$${revision}"
+    "-X main.version=$${version}"
     "-X main.time=${PKG_TIME}"
   ];
 
-  vendorHash = null;
+  meta = {
+    homepage = "${PKG_HOMEPAGE}";
+    description = "${PKG_DESC}";
+    changelog = "https://github.com/${PKG_OWNER}/${PKG_REPO}/releases/tag/v$${version}";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ ${PKG_OWNER} ];
+    mainProgram = "${PKG_REPO}";
+  };
 }
