e1e6321497c8cb1cfaf1c6e7f92e06ff7e000573
Antti Harju <antti@harju.io>
2026-02-07 11:37:25 +0200

    Fix local use of archive.sh

--- .release/archive.sh
+++ .release/archive.sh
@@ -4,12 +4,14 @@ repo_root="$(git rev-parse --show-toplevel)"
 cd "$repo_root"
 
 tag="$1"
-target="$2"
+os="$2"
+arch="$3"
+target="$os-$arch"
 echo "$0 $tag $target"
 
 remote_url="$(git remote get-url origin)"
 repo="$(basename --suffix .git "$remote_url")"
-go build -ldflags "-s -w -buildid=github-$tag" -trimpath -o "target/$target/release/$repo"
+GOOS="$os" GOARCH="$arch" go build -ldflags "-s -w -buildid=github-$tag" -trimpath -o "target/$target/release/$repo"
 
 cd "target/$target/release"
 tar -czf "$repo_root/$repo-$target.tar.gz" "$repo"
